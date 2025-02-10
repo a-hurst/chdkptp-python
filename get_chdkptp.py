3412c332c22908d91eee53b9834e1556e05c6de1
Austin Hurst <mynameisaustinhurst@gmail.com>
2025-02-10 18:08:49 -0400

    Use chdkptp-built static Lua for build process

--- get_chdkptp.py
+++ get_chdkptp.py
@@ -1,6 +1,8 @@
 import os
 import io
+import sys
 import shutil
+import subprocess as sub
 from zipfile import ZipFile
 
 try:
@@ -25,3 +27,19 @@ def get_chdkptp_source(outdir):
     with ZipFile(data, 'r') as z:
         os.mkdir(outdir)
         z.extractall(path=outdir)
+
+
+def apply_patches(srcdir, patchfile):
+    sub.check_call(['patch', '-d', srcdir, '-i', patchfile, '-p', '1'])
+
+
+def build_static_lua(srcdir):
+    orig_path = os.getcwd()
+    misc_path = os.path.join(srcdir, 'misc')
+    os.chdir(misc_path)
+    cmd = ['bash', 'setup-ext-libs.bash', '-nogui']
+    p = sub.Popen(cmd, stdout=sys.stdout, stderr=sys.stderr)
+    p.communicate()
+    if p.returncode != 0:
+        raise RuntimeError("Error building static Lua")
+    os.chdir(orig_path)
