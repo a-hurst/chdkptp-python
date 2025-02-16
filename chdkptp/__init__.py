c2ed1c84621c4aa4733c43465e4023db58a6307f
Austin Hurst <mynameisaustinhurst@gmail.com>
2025-02-16 15:37:02 -0400

    Export PTPError in main package

--- chdkptp/__init__.py
+++ chdkptp/__init__.py
@@ -1,4 +1,4 @@
-from chdkptp.device import ChdkDevice, list_devices, DeviceInfo
+from chdkptp.device import ChdkDevice, list_devices, DeviceInfo, PTPError
 
 __version__ = "0.1.3"
-__all__ = ['ChdkDevice', 'list_devices', 'DeviceInfo']
+__all__ = ['ChdkDevice', 'list_devices', 'DeviceInfo', 'PTPError']
